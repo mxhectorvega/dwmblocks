f8422e95c06b8d09ed97645f58e9233611f03eff
mxhectorvega <mxhectorvega@gmail.com>
2021-04-22 13:50:26 -0500

    Actualizacion de scripts para statusbar jue 22 abr 21 13:50

--- blocks.h
+++ blocks.h
@@ -2,23 +2,26 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
 
-	{" ", "music",	1,	0},
+	{" ", "music",		1,	0},
 
-	{" ", "clima",	60,	5},
+	{" ", "clima",		60,	5},
 
-	{" ", "wifi",	60,	5},
+	{" ", "wifi",		60,	5},
 
-/*	{" ", "cpu",	5,	5}, */
-	
-	{" ", "memory",	5,	5},
+	{" ", "cpu",		5,	5}, 
 
-	{" ", "volume",	1,	5},
+	{" ", "carga",		5,	5}, 
 
-	{" ", "clock",	1,	0},
+	{" ", "bateria",	5,	5}, 
+
+	{" ", "brillo",		5,	5}, 
+
+	{" ", "memory",		5,	5},
+
+	{" ", "volume",		1,	5},
+
+	{" ", "clock",		1,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
 static char delim = '\0';
-
-
-
